27160b4104716c01dba78a2acebebba27adadce4
Paul Rosero <paul_rosero@yahoo.com>
2019-04-10 01:06:46 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,11 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts[:favorite_ice_cream_flavors].delete_if {|:favorite_ice_cream_flavors, :ice_cream| ice_cream == "strawberry"}
-
+  contacts.each do |person, contact_details_hash|
+  contact_details_hash.each do |attribute, data|
+    if attribute == :favorite_ice_cream_flavors
+      data.delete_if {|ice_cream| ice_cream == "strawberry"}
+    end
+  end
+end
 end
